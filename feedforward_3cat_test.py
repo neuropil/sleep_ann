44240c4bad62033bab25ea347bf1dc1e0450a913
Elijah C <ejd.christensen@gmail.com>
2017-09-15 02:45:11 -0600

    need to write own cross_val_score method

--- feedforward_3cat_test.py
+++ feedforward_3cat_test.py
@@ -13,9 +13,18 @@ from sklearn.metrics import precision_score,recall_score,make_scorer
 Preprocessed datafiles are mat files with the following keys:
 bands : ndarray
 '''
-osx_data_path = '/Users/elijahc/data/uminn/preprocessed'
-all_data = load_preprocessed(data_path=osx_data_path,simple=True)
-merged_data = load_preprocessed(data_path=osx_data_path,merge_keys=['pows','stages'],simple=True)
+# osx_data_path = '/Users/elijahc/data/uminn/preprocessed'
+# all_data = load_preprocessed(data_path=osx_data_path,simple=True)
+# merged_data = load_preprocessed(data_path=osx_data_path,merge_keys=['pows','stages'],simple=True)
+file_names=[
+    'band_pow1.mat',
+    'band_pow2.mat',
+    'band_pow3.mat',
+    'band_pow4.mat',
+    'band_pow5.mat',
+    ]
+all_data = load_preprocessed(file_names=file_names,simple=True)
+merged_data = load_preprocessed(file_names=file_names,merge_keys=['pows','stages'],simple=True)
 
 scaler = StandardScaler()
 scaler.fit(merged_data['pows'])
@@ -27,7 +36,8 @@ model_params = dict(
 )
 fit_params = dict(
     validation_split=0.1,
-    epochs=500,
+    epochs=250,
+    verbose=1,
 )
 cvs = prepare_cvs(model=feedforward,cv='sss',
     scoring=[make_scorer(precision_score)],
@@ -40,6 +50,7 @@ merge_stages = merged_data['stages_simple']
 merge_pows = get_pow_bands(merged_data,scaler)
 merge_stages_oh = get_oh_labels(merge_stages)
 merge_sample_weights = get_inverse_freq_weights(merge_stages)
+fit_params['sample_weight'] = merge_sample_weights
 pp(cvs)
 results = cross_val_score(**cvs,X=merge_pows,y=merge_stages_oh,verbose=1,fit_params=fit_params)
 import ipdb; ipdb.set_trace()
